614fee385d7cd2ab794eb1c9428560dfb93d8637
fineYoungchan <naddana2@gmail.com>
2020-08-10 11:34:07 +0900

    [ENH] lide it

--- 1-vanilla/js/models/HistoryModel.js
+++ 1-vanilla/js/models/HistoryModel.js
@@ -1,25 +1,27 @@
 export default {
   data: [
     { keyword: '검색기록2', date: '12.03' },
-    { keyword: '검색기록1', date: '12.02'},
+    { keyword: '검색기록1', date: '12.02' },
     { keyword: '검색기록0', date: '12.01' },
   ],
 
+  /** 데이터를 return 한다. 서버에서 비동기로 데이터를 가져올수 있음, 쿠키로 데이터를 얻을 수 있음 */
   list() {
     return Promise.resolve(this.data)
   },
-  
+
+  /** 키워드가 존재하면 기존데이터 삭제 후 날짜를 주입하여 키워드를 다시 세팅한다. */
   add(keyword = '') {
     keyword = keyword.trim()
-    if (!keyword) return 
+    if (!keyword) return
     if (this.data.some(item => item.keyword === keyword)) {
       this.remove(keyword)
     }
 
     const date = '12.31'
-    this.data = [{keyword, date}, ...this.data]
+    this.data = [{ keyword, date }, ...this.data]
   },
-  
+
   remove(keyword) {
     this.data = this.data.filter(item => item.keyword !== keyword)
   }
--- 1-vanilla/js/views/View.js
+++ 1-vanilla/js/views/View.js
@@ -1,6 +1,7 @@
 const tag = '[View]'
 
 export default {
+  /**  엘리멘트를 주입받아 자신의 프로퍼티로 */
   init(el) {
     if (!el) throw el
     this.el = el
@@ -11,7 +12,7 @@ export default {
     this.el.addEventListener(event, handler)
     return this
   },
-
+  /** 엘리먼트는 이벤트를 방출함 */
   emit(event, data) {
     const evt = new CustomEvent(event, { detail: data })
     this.el.dispatchEvent(evt)
